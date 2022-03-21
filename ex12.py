73bf346b9ae368c6f51d4ef43ca666e6c4e2be5e
lucaslpss <100217394+lucaslpss@users.noreply.github.com>
2022-03-21 12:12:23 -0300

    Delete ex12.py

--- ex12.py
+++
@@ -1,4 +0,0 @@
-salario = float(input("Digite o salário do funcionário"))
-aumento = salario * (25/100)
-novoSalario = salario + aumento
-print("O novo salário do funcionário apos o aumento é: ", novoSalario)

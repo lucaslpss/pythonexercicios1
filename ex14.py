ba3fa48ad5ba14e6e42f2c5a3b803a87e882a4fa
lucaslpss <100217394+lucaslpss@users.noreply.github.com>
2022-03-21 12:12:35 -0300

    Delete ex14.py

--- ex14.py
+++
@@ -1,9 +0,0 @@
-totalCabeças = int(input("Digite o total de cabeças no cercado"))
-totalPatas = int(input("Digite o total de patas no cercado"))
-max = (totalCabeças * 4)
-min = (totalCabeças * 2)
-if totalCabeças > max:
-    print("Inválido")
-if totalCabeças < min:
-    print ("Inválido")
-else: print("Ok, prosseguir")

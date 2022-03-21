6ded4b4cd9d0626fa08891ce5d5520beab097f60
lucaslpss <100217394+lucaslpss@users.noreply.github.com>
2022-03-21 12:12:28 -0300

    Delete ex13.py

--- ex13.py
+++
@@ -1,5 +0,0 @@
-n1 = int(input("Digite a sua primeira nota"))
-n2 = int(input("Digite a sua segunda nota"))
-n3 = int(input("Digite a sua terceira nota"))
-media = (n1+n2+n3)/3
-print("A média aritmética das suas notas é ", media)
